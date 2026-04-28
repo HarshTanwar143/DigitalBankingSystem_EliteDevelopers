688b264f89b036763bfdb33c8f31250b6f7b39b7
Diksha <diksha11e@gmail.com>
2026-04-28 12:22:54 +0530

    adding Accounts Controller

--- SmartBank_Sprint1_FIXED/SmartBank.Data/obj/Debug/net9.0/SmartBank.Data.AssemblyInfo.cs
+++ SmartBank_Sprint1_FIXED/SmartBank.Data/obj/Debug/net9.0/SmartBank.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartBank.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30fd59904c2304777b1b2fb66f1d6577afd2b9d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartBank.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartBank.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
